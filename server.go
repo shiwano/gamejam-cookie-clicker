6352ef2ec0a3cce27febb7484a7ffbed25cdcb03
Shogo Iwano <shiwano@gmail.com>
2016-08-15 01:39:02 +0900

    :hourglass:

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shiwano/websocket-conn"
+	"net/http"
+)
+
+func runServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		c := conn.New()
+		c.TextMessageHandler = func(text string) {
+		}
+		if err := c.UpgradeFromHTTP(w, r); err != nil {
+			fmt.Printf("Failed to connect: %v", err)
+			return
+		}
+		fmt.Println("Client connected")
+	})
+	http.ListenAndServe(":5000", nil)
+}
